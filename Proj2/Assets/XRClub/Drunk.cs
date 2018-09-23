1138cc2210ba6b42721700dc4e3888c2c2399702
minersail <jkwoo1234@gmail.com>
2018-09-23 18:57:40 -0400

    project 2 scenes 1/2

---
+++ Proj2/Assets/XRClub/Drunk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drunk : MonoBehaviour
+{
+	void Update ()
+    {
+        GetComponent<Animator>().SetBool("Falling", transform.position.y < 0);
+
+        // ---START CODE---
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("Malcolm's brain isn't working very well right now. Help him not die!");
+
+            // MULTIPLE WAYS TO DO THIS:
+
+            transform.Rotate(0, 90, 0);
+
+            // transform.eulerAngles = new Vector3(0, 90, 0);
+
+            // transform.Translate(0, 0, -1);
+
+            // Vector3 position = transform.position;
+            // position.z -= 1;
+            // transform.position = position;
+        }
+
+        // ---END CODE---
+	}
+}
