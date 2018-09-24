8e79898b087a680bfb5a2756b2e5e7ca93c73e46
minersail <jkwoo1234@gmail.com>
2018-09-23 21:44:43 -0400

    quick save

--- Proj2/Assets/Scripts/Scene2/BowlingBall.cs
+++ Proj2/Assets/Scripts/Scene2/BowlingBall.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class BowlingBall : MonoBehaviour
 {
+    /* Notes
+     * 
+     */
+
 	// This function runs once, at the beginning of the simulation.
 	void Start ()
     {
-        GetComponent<Rigidbody>().maxAngularVelocity = 100;
-        //Debug.Log(GetComponent<Rigidbody>().angularVelocity);
-	}
+        //GetComponent<Rigidbody>().maxAngularVelocity = 100;
+        //GetComponent<Rigidbody>().mass;
+        //GetComponent<Rigidbody>().drag;
+        //GetComponent<Rigidbody>().angularDrag;
+        //GetComponent<Rigidbody>().useGravity;
+        //GetComponent<Rigidbody>
+    }
 }
