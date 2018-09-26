1ec99db2e8e3e74c8fc96efcf22ea487686c3269
Jordan Woo <jkwoo1234@gmail.com>
2018-09-26 17:37:42 -0400

    added some instructions

--- Proj2/Assets/Scripts/Scene3/MoveDrunk.cs
+++ Proj2/Assets/Scripts/Scene3/MoveDrunk.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class MoveDrunk : MonoBehaviour
 {
+    public float speed;
+
     // This function runs once, at the beginning of the level.
 	void Start ()
     {
-        /* Let's save Malcolm!
-         *  There are multiple ways to do this.
-         *  
-         */
+        
+    }
 
-        // 1: Rotate 
-        // transform.Rotate(0, 90, 0);
+    // This code runs once per frame.
+    void Update()
+    {
+        /* Let's save Malcolm!
+         */
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            /* This code will run on any frame that the space button is initially pressed down.
+             * 
+             * Two basic solutions are commented out below - see if you can figure out others
+             */
+
+            // Note: you can just use "transform" as a shortcut instead of "GetComponent<Transform>().
+            // 1: Rotate him 90 degrees
+            // transform.Rotate(0, 90, 0);
+
+            // 2: Set his drag really high
+            // GetComponent<Rigidbody>().drag = 5000;
+        }
+
 
         // transform.eulerAngles = new Vector3(0, 90, 0);
 
@@ -26,8 +45,6 @@ public class MoveDrunk : MonoBehaviour
         // MEME ANSWERS
 
         // GetComponent<Rigidbody>().useGravity = false;
-
-        // GetComponent<Rigidbody>().drag = 5000;
 
         // GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY & RigidbodyConstraints.FreezeRotationZ;
 
---
+++ Proj2/README.md
@@ -0,0 +1,12 @@
+GameObject/Component
+Component/Monobehavior - every script is a component
+
+.gameObject/GetComponent() - Accessing gameobject through component, accessing component through gameobject
+Accessing component variables through scripting - GetComponent<>().useGravity, etc.
+
+Start()/Update() - get game loop
+
+Exposing variables to the inspector - variables don't save in play mode, inspector values override default values in script
+Accessing variables from different scripts - back to every script is a component
+
+Vector3? Time.deltaTime?
