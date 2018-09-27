eebdf8ec9d716e2eb9b8f1001021697194d6031b
minersail <jkwoo1234@gmail.com>
2018-09-27 00:49:42 -0400

    added animations

---
+++ Proj2/Assets/Scripts/SaveMalcolm.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveMalcolm : MonoBehaviour
+{
+    // This function runs once, at the beginning of the level.
+	void Start ()
+    {
+        // We're using update instead, because we need to check if the space bar is pressed down every frame, not just the first one.
+    }
+
+    // This code runs once per frame.
+    void Update()
+    {
+        /* Let's save Malcolm!
+         */
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            /* This code will run on any frame that the space button is initially pressed down.
+             * 
+             * Two basic solutions are commented out below - see if you can figure out others
+             */
+
+            // Note: you can just use "transform" as a shortcut instead of "GetComponent<Transform>()".
+            // 1: Rotate him 90 degrees
+            // transform.Rotate(0, 90, 0);
+
+            // 2: Set his drag really high
+            // GetComponent<Rigidbody>().drag = 5000;
+
+            /* Think of other ways to save Malcolm!
+             */
+        }
+    }
+}
--- Proj2/Assets/XRClub/Drunk.cs
+++ Proj2/Assets/XRClub/Drunk.cs
@@ -7,5 +7,27 @@ public class Drunk : MonoBehaviour
 	void Update ()
     {
         GetComponent<Animator>().SetBool("Falling", transform.position.y < 0);
+
+        // ANSWERS for saving malcolm
+        // transform.eulerAngles = new Vector3(0, 90, 0);
+
+        // transform.Translate(0, 0, -1);
+
+        // Vector3 position = transform.position;
+        // position.z -= 1;
+        // transform.position = position;
+
+        // MEME ANSWERS
+
+        // GetComponent<Rigidbody>().useGravity = false;
+
+        // GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY & RigidbodyConstraints.FreezeRotationZ;
+
+        // transform.Rotate(0, 1, 0);
+
+        // GetComponent<CapsuleCollider>().direction = 2;
+        // GetComponent<CapsuleCollider>().height += 5;
+
+        // GetComponent<Animator>().enabled = false;
     }
 }
