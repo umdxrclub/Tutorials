5235206feda6bd749fbb6a07bac04d21d38d38d9
minersail <jkwoo1234@gmail.com>
2018-10-02 18:27:04 -0400

    proj3 started

---
+++ Proj3/Assets/MoveSphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour
+{
+    Rigidbody body;
+
+	void Start ()
+    {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        body.AddForce(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"), ForceMode.Force);
+	}
+}
---
+++ Proj3/Assets/SwitchColor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchColor : MonoBehaviour
+{
+    static int WHITE;
+    static int YELLOW;
+    static int GREEN;
+    static int BLUE;
+
+    void Start()
+    {
+        WHITE = LayerMask.NameToLayer("Default");
+        YELLOW = LayerMask.NameToLayer("Yellow");
+        GREEN = LayerMask.NameToLayer("Green");
+        BLUE = LayerMask.NameToLayer("Blue");
+    }
+
+    void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int color = gameObject.layer;
+
+            if (color == WHITE || color == YELLOW)
+            {
+                color = GREEN;
+                //GetComponent<MeshRenderer>().material = 
+            }
+            else if (color == GREEN)
+            {
+                color = BLUE;
+            }
+            else if (color == BLUE)
+            {
+                color = YELLOW;
+            }
+
+            gameObject.layer = color;
+        }
+	}
+}
