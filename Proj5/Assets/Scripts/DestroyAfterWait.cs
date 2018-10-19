1301583ed2584637185d64b40af7adb449e6dc33
jlball <32344022+jlball@users.noreply.github.com>
2018-10-19 00:35:56 -0400

    Update 1
    First Update of John working on the project - added two scripts to be written by the students, ExplosionRaycast and DestoryAfterWait, still need to add custom ExplosionForce script and actual mechanics.

---
+++ Proj5/Assets/Scripts/DestroyAfterWait.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterWait : MonoBehaviour {
+
+    public float lifetime = 5.0f;
+
+    private float counter;
+
+	void Update () {
+        counter += Time.deltaTime;
+        if (counter >= lifetime)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+}
