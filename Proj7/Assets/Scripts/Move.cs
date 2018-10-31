664f246f383a3c9fef1df222cc704637c14ab96c
gogittossjx <fatai.fakoya@gmail.com>
2018-10-31 16:51:08 -0400

    added project 7

---
+++ Proj7/Assets/Scripts/Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.Translate(movement * Time.deltaTime * speed);
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        // WRITE CODE BELOW HERE ------------------------------
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Jump");
+        }
+
+        anim.SetFloat("Speed", movement.magnitude);
+	}
+}
